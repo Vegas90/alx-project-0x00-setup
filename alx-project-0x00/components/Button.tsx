4c0d3da180922b3a7893b3bca58c648a76a68c16
Vegas90 <amosmwoni@gmail.com>
2025-05-28 17:03:00 +0300

    m

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,9 +1,6 @@
 //Create a button component that accepts title and styles as an argument. Create multiple instances (sizes: small, medium, Large - shapes: rounded-sm, rounded-md, rounded-full) of the button component under the /landing paths.
-import React from 'react';
-interface ButtonProps {
-  title: string;
-  styles?: string;
-}   
+import { ButtonProps } from "@/interfaces"; 
+//argument: title, styles 
 const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
     <button className={`px-4 py-2 text-white bg-blue-500 rounded ${styles}`}>
