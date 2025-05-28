0084d99581ee509024f052776171c188112d29d7
Vegas90 <amosmwoni@gmail.com>
2025-05-28 17:08:56 +0300

    m

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,7 +5,12 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button title={""} styles={""}/>
+      <div className="flex gap-4 mt-4">
+        <Button title="Small Button" styles="rounded-sm" />
+        <Button title="Medium Button" styles="rounded-lg" />
+        <Button title="Large Button" styles="rounded-full" /> 
+        <button/>
+    </div>
     </div>
   )
 }
