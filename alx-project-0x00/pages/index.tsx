9d795b4c8d13725ec495339be3015ea0a3fab29b
Vegas90 <amosmwoni@gmail.com>
2025-05-28 15:53:03 +0300

    h1

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -22,6 +22,7 @@ const Home: React.FC = () => {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
+      <h1>Airbnb Application Clone system</h1>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
